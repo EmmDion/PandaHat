f319d25af8c82f322a945ecf6e00d1eb60e80b14
windowpain <windowpain@users.noreply.github.com>
2015-11-14 20:25:58 -0800

    Updated in-line documentation

--- php/Fetch.php
+++ php/Fetch.php
@@ -1,7 +1,13 @@
 <?php
 
-// Authors:  Panda_S (Panda_X), and Panda_M
+// Project:   PandaHat > Iter1 backend php > Fetch.php
+// Authors:   Panda_S (Panda_X), and Panda_M
+// Created:   2015-11-12
+// Modified:  2015-11-14
 
+// See note in 'Give.php' about the use of mysqli.
+
+// Defines $server, $user, $pass, $dbname, and $port.
 include('connectionData.txt');
 
 $conn = mysqli_connect($server, $user, $pass, $dbname, $port)
@@ -25,6 +31,8 @@ or die('Error connecting to MySQL server.');
 $survey_tag = $_POST['survey_tag'];
 $student_id = $_POST['student_id'];
 
+
+// Fetch survey results for one survey and one student. Also, list in sorted order.
 
 //TODO: rename quiz_tag to survey_tag
 $query = "SELECT * FROM survey_results WHERE quiz_tag = '$survey_tag' AND student_id = '$student_id' ORDER BY question_num";
@@ -50,11 +58,15 @@ Result of query:
 
 print "<pre>";
 
+// The mysqli way of querying the database.
 if($stmt = $conn -> prepare($query)){
 
 
 	$stmt -> execute();
+	// Identifiers post-fixed with 'fetch' - otherwise wasn't sure if would cause clash.
 	$stmt -> bind_result($student_id_fetch, $survey_tag_fetch, $question_num_fetch, $result_fetch);
+
+    // Currently data is output in HTML. Will change to JSON in future.
 	echo "<table style=\"width:50%\">";
 	while($stmt -> fetch()){
 		echo "\n";
--- php/Give.php
+++ php/Give.php
@@ -1,6 +1,18 @@
 <?php
 
-// Authors:  Panda_S (Panda_X), and Panda_M
+// Project:   PandaHat > Iter1 backend php > Give.php
+// Authors:   Panda_S (Panda_X), and Panda_M
+// Created:   2015-11-12
+// Modified:  2015-11-14
+
+
+// In Iter1 code somehow uses mysqli instead of mysql.
+//
+// It is planned that the MeekroDB library interface will be used in the future.
+//
+// Panda_M couldn't get hello world to work. Panda_S provided an example of code
+// which worked (and incidentally relies on mysqli), so the following code was
+// built using that example as a template.
 
 include('connectionData.txt');
 
@@ -21,63 +33,46 @@ or die('Error connecting to MySQL server.');
 	
 	
 <?php
-	
-//$manu = $_POST['manu'];
 
+$numberOfQuestions = 3;  // Should probably store this somewhere else.
 
-/*
-$query = "INSERT INTO survey_results VALUE ('textid', 'texttag', 42, 'textresult')";
-if($stmt = $conn -> prepare($query))
-{
-	$stmt -> execute();
-	echo "The query has executed.";
-}
-else
-{
-	echo "The query did not execute.";
-}
-*/
+//$questionIndexMap = array();  // An attempt at mechanism to 'factor out' the name conventions from the code.
 
-//$successTest = mysql_query($query);
-//$testSuccess = (boolean) $stmt;
-//echo "<p>". ($testSuccess ? "Executed" : "Failed") . "</p>";
-
-$numberOfQuestions = 3;
-//$questionIndexMap = array();
 for ($i = 0; $i < $numberOfQuestions; $i++)
 {
 	// Some mapping between naming conventions.
-	$paramName = "q00" . strval($i); //FIXME - need leading zeros
+	$paramName = "q00" . strval($i); //FIXME - need leading zeros (Iter1)
 
+    //DEBUG:
 	echo "We are inside the loop.";
 
-    //$query = "INSERT INTO survey_results VALUE ('textid', 'texttag', 42, 'textresult')";
+	//DEBUG:
+    //$query = "INSERT INTO survey_results VALUE ('arbitrary_textid', 'arbitrary_texttag', 42, 'arbitrary_textresult')";
+
     $query = "INSERT INTO survey_results VALUE ('{$_POST['student_id']}', '{$_POST['survey_tag']}', $i, '{$_POST[$paramName]}');";
 
+    //DEBUG:
     echo "The query is: $query" . "<br>";
 
-    //$successTest = mysql_query($query);
-    //echo "<p>". ($successTest ? "Executed" : "Failed") . "</p>";
-
+    // The mysqli way of querying the database.
 	if($stmt = $conn -> prepare($query))
 	{
 		$stmt -> execute();
+	    //DEBUG:
 		echo "That executed.";
 	}
 	else
 	{
+	    //DEBUG:
 		echo "That didn't work out.";
 	}
 
-
-	//$questionIndexMap[]
 }
 
 
-//$query = "SELECT * FROM survey_results";
-
 ?>
 
+<!-- Something to show the user once we get done with the script. -->
 <p> Thank you! You are now assuming that the script has worked. Congratulations. </p>
 
 
