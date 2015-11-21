ad17084dbc69128685b0c79c6bd02a37f1c4ef1e
windowpain <windowpain@users.noreply.github.com>
2015-11-21 14:33:11 -0800

    Done studentlogin.php (issue: need timezone)

--- php/studentlogin.php
+++ php/studentlogin.php
@@ -1,83 +1,82 @@
 <?php
 
 // Project:   PandaHat > Iter2 backend php > studentlogin.php
-// Authors:   Panda_S (Panda_X), and Panda_M
+// Authors:   Panda_M
 // Created:   2015-11-18
-// Modified:  2015-11-18
-// Buggy? :   Yes, could be anywhere between connecting with DB to fetching query result.
+// Modified:  2015-11-21
+// Issues:    Timezone is not considered yet; as of now, system manipulates pure UTC.
 
-//BUG: Query gives null result (even with no constraint on returned rows).
+
+// ------------------------------------------------------------------
+
+// Initialize return value to the failure message state.
+$return_value = json_decode('{"debug": null, "data": {"survey_id": null, "success": false} }');
+
+// Change debug member of return value, then get string rep of return value.
+//TODO: move to a utilities file.
+function bake_with_debug($mutable_return_value, $debug_string)
+{
+    $mutable_return_value->debug = $debug_string;
+    return json_encode($mutable_return_value);
+}
+
+// ------------------------------------------------------------------
 
 // Defines $server, $user, $pass, $dbname, and $port.
 include('connectionData.txt');
 
-
-//DEBUG:
-echo "Here is the beginning of the script.";
-
+// Connect to SQL database.
 $conn = mysqli_connect($server, $user, $pass, $dbname, $port)
-or die('Error connecting to MySQL server.');
+or die(bake_with_debug($return_value, 'Error connecting to MySQL server.'));
 
+// ------------------------------------------------------------------
+
+// Read _POST parameter.
 $survey_tag = $_POST['survey_tag'];
 
-
 // Fetch survey_id, and start and end times for survey with given survey_tag.
+$query = "SELECT survey_id, start_date, end_date FROM credentials_stu WHERE survey_tag = '$survey_tag'";
 
-//TODO: rename quiz_tag to survey_tag
-//$query = "SELECT survey_id, start_date, end_date FROM credentials_stu WHERE survey_tag = '$survey_tag'";
-$query = "SELECT survey_id, start_date, end_date FROM credentials_stu";
-
-
-//DEBUG:
-echo "<br><br>";
-echo $query;
-echo "<br><br>";
-
-// Initialize return value.
-$return_value = json_decode('{"debug": null, "data": {"survey_id": null, "success": false} }');
-
-// The mysqli way of querying the database.
+// (The mysqli way of querying the database.)
 if($stmt = $conn -> prepare($query)){
-
-    //DEBUG:
-    //echo "preparation succeeded.";
-
     $stmt -> execute();
     $stmt -> bind_result($survey_id, $start_date, $end_date);
 
-    // Use $stmt->fetch() to put results in bound variables.
+    // Populate variables with query results, as bound above.
     $stmt->fetch();
 
     // Check if the SQL results are nonempty--i.e., user entered a valid survey_tag.
     if (!is_null($survey_id))
     {
-        // Check start_date & end_date, yield survey_id.
-        //$now = time();
-        //echo $now;
+        // Credentials ok.
+        $return_value->data->survey_id = $survey_id;
 
-        //DEBUG:
-        echo "survey_tag is valid";
-    }
-    else
-    {
-        // Return failure, bad login.
+        // Get times in UTC ints - Note: no timezone checking yet.
+        $now = time();
+        $start = strtotime($start_date);
+        $end = strtotime($end_date);
 
-        //DEBUG:
-        echo "survey_tag is not valid";
+        // Check start_date & end_date.
+        if ($start <= $now && $now <= $end)
+        {
+            $return_value->data->success = TRUE;
+        }
     }
+    //else
+    //{
+        // Bad login, keep failure state & return.
+    //}
 
     $stmt->close();
 }
-//DEBUG:
 //else
 //{
-    //echo "preparation failed.";
+    // Preparation failed.
 //}
 
 
 // Ouptut in JSON
 echo json_encode($return_value);
-
 
 mysqli_close($conn);
 
