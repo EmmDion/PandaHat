73af78b03f549796c102f13bfc5f973a4302734d
windowpain <windowpain@users.noreply.github.com>
2015-11-21 19:39:46 -0800

    Did instructorlogin.php BIG SECURITY HOLE (which MeekroDB will fix)

--- php/instructorlogin.php
+++ php/instructorlogin.php
@@ -4,7 +4,10 @@
 // Authors:   Panda_M
 // Created:   2015-11-21
 // Modified:  2015-11-21
-// Buggy? :   Yes, probably error around database connection.
+// Buggy? :   Major security hole - if user enters '*' as user and password (single-quotes included),
+//            then they can get in without even knowing the login credentials.
+//            [Suggested temp fix]: Use mysqli stmt::bind_param instead of quoting input.
+//            [Suggested better fix]: The MeekroDB library interface may take care of this.
 
 
 // ------------------------------------------------------------------
@@ -31,15 +34,14 @@ or die(bake_with_debug($return_value, 'Error connecting to MySQL server.'));
 
 // ------------------------------------------------------------------
 
-// Read _POST parameter.
+// Read _POST parameters.
 $param_user = $_POST['user'];
 $param_pass = $_POST['pass'];
 
-// Fetch survey_id, and start and end times for survey with given survey_tag.
+// Fetch user for record with given user & pass.
+// We don't actually need the fetched value (we already have it), but we need
+// to know if the return value of the query is non-null.
 $query = "SELECT user FROM credentials_pro WHERE user = '$param_user' AND pass = '$param_pass'";
-
-//DEBUG:
-echo "The query was '$query'";
 
 // (The mysqli way of querying the database.)
 if($stmt = $conn -> prepare($query)){
@@ -49,8 +51,8 @@ if($stmt = $conn -> prepare($query)){
     // Populate variables with query results, as bound above.
     $stmt->fetch();
 
-    // Check if the SQL results are nonempty--i.e., user entered a valid survey_tag.
-    if (!is_null($result_user)
+    // Check if the SQL results are nonempty--i.e., user entered a valid login.
+    if (!is_null($result_user))
     {
         // Credentials ok.
         $return_value->data->success = TRUE;
